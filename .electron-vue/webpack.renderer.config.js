c92ee22206a8831d832eed2fd93599cdbc074c2b
高新凯 <467469274@qq.com>
2018-06-14 17:36:29 +0800

    2018年06月14日17:36:29

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -122,7 +122,8 @@ let rendererConfig = {
   resolve: {
     alias: {
       '@': path.join(__dirname, '../src/renderer'),
-      'vue$': 'vue/dist/vue.esm.js'
+        'common':path.resolve(__dirname, '../src/common'),
+        'vue$': 'vue/dist/vue.esm.js'
     },
     extensions: ['.js', '.vue', '.json', '.css', '.node']
   },
