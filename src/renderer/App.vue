039ec01aad9d683071ff43c54e822023597ec40d
高新凯 <467469274@qq.com>
2018-06-14 19:28:46 +0800

    2018年06月14日19:28:46

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -9,11 +9,16 @@
     var pouchPath = (os.type()=='Windows_NT')?process.cwd():'/tmp';
     alert(pouchPath)
     var PouchDB = require('pouchdb-browser');
-    var music = new PouchDB(pouchPath+'/muisc');
+    var music = new PouchDB(pouchPath+'/data/');
   export default {
     name: 'my-project',
-
       created(){
+          music.destroy().then(function (response) {
+              console.log(music)
+              console.log('成功清除全部缓存')
+          }).catch(function (err) {
+              console.log(err)
+          });
       }
   }
 </script>
